f095fe68611e1f4bbf73827b9a30aeb9c27bb8e1
jedlloren <jed.lloren@apexrms.com>
2021-08-19 17:09:43 -0400

    Update changing-raster-extent.R
    Edited my description of how the new extents were calculated. Added for different metadata stages along the process of creating a new raster.

--- Scripts/changing-raster-extent.R
+++ Scripts/changing-raster-extent.R
@@ -21,14 +21,31 @@ primaryStratum<-raster("primaryStratum.tif")
 # I will be referring to this raster as "original raster" throughout
 primaryStratum
 
+# class      : RasterLayer 
+# dimensions : 3175, 4399, 13966825  (nrow, ncol, ncell)
+# resolution : 90, 90  (x, y)
+# extent     : -491430, -95520, 117930, 403680  (xmin, xmax, ymin, ymax)
+# crs        : +proj=lcc +lat_0=44 +lon_0=-68.5 +lat_1=46 +lat_2=60 +x_0=0 +y_0=0 +ellps=GRS80 +units=m +no_defs 
+# source     : PrimaryStratum.tif 
+# names      : PrimaryStratum 
+# values     : -32768, 32767  (min, max)
+
 # Create a new raster named OutaouaisConnectivityExtent
 # ymx and ymn values were retained from the primaryStratum raster
-# xmx and xmn were calculated manually where 1km = 1000 pixels
+# xmx (eastern extent) was moved 295.89km west
+# xmn (western extent) was moved 199.98km west
 # ncol and nrow were calulated using (xmx-xmn)/90 and (ymx-ymn)/90, respectively
+# This new raster has an area of 85,716.4275 km^2
 OutaouaisConnectivityExtent <- raster(ncol=3333, nrow=3175, xmn=-691380, xmx=-391410, ymn=117930, ymx=403680)
 
 # Check your raster metadata
 OutaouaisConnectivityExtent
+
+# class      : RasterLayer 
+# dimensions : 3175, 3333, 10582275  (nrow, ncol, ncell)
+# resolution : 90, 90  (x, y)
+# extent     : -691380, -391410, 117930, 403680  (xmin, xmax, ymin, ymax)
+# crs        : NA 
 
 # Give each of your cells a value
 # We'll just be giving the entire raster a value of 1
@@ -36,6 +53,15 @@ values(OutaouaisConnectivityExtent) <- rep(1, ncell(OutaouaisConnectivityExtent)
 
 # Check your raster metadata to see if it worked
 OutaouaisConnectivityExtent
+
+# class      : RasterLayer 
+# dimensions : 3175, 3333, 10582275  (nrow, ncol, ncell)
+# resolution : 90, 90  (x, y)
+# extent     : -691380, -391410, 117930, 403680  (xmin, xmax, ymin, ymax)
+# crs        : NA 
+# source     : memory
+# names      : layer 
+# values     : 1, 1  (min, max)
 
 # Plot your raster to see if it works
 x11(); plot(OutaouaisConnectivityExtent)
@@ -46,8 +72,26 @@ crs(OutaouaisConnectivityExtent) <- crs(primaryStratum)
 # Look at your raster metadata to see if it worked
 OutaouaisConnectivityExtent
 
+# class      : RasterLayer 
+# dimensions : 3175, 3333, 10582275  (nrow, ncol, ncell)
+# resolution : 90, 90  (x, y)
+# extent     : -691380, -391410, 117930, 403680  (xmin, xmax, ymin, ymax)
+# crs        : +proj=lcc +lat_0=44 +lon_0=-68.5 +lat_1=46 +lat_2=60 +x_0=0 +y_0=0 +ellps=GRS80 +units=m +no_defs 
+# source     : memory
+# names      : layer 
+# values     : 1, 1  (min, max)
+
 # Compare with the original raster
 primaryStratum
+
+# class      : RasterLayer 
+# dimensions : 3175, 4399, 13966825  (nrow, ncol, ncell)
+# resolution : 90, 90  (x, y)
+# extent     : -491430, -95520, 117930, 403680  (xmin, xmax, ymin, ymax)
+# crs        : +proj=lcc +lat_0=44 +lon_0=-68.5 +lat_1=46 +lat_2=60 +x_0=0 +y_0=0 +ellps=GRS80 +units=m +no_defs 
+# source     : PrimaryStratum.tif 
+# names      : PrimaryStratum 
+# values     : -32768, 32767  (min, max)
 
 # Export the resulting raster
 writeRaster(OutaouaisConnectivityExtent, "C:/Enter/File/Path/OutaouaisConnectivityExtent.tif", format = "GTiff", overwrite=TRUE)
