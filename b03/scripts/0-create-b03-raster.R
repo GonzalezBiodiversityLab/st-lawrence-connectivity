e84e011c5bd2f74a940f3e25690d9f3f66017789
jedlloren <jed.lloren@apexrms.com>
2021-10-07 11:14:55 -0400

    Create 0-create-b03-raster.R
    Created R script used to rasterize the B03 study area.

---
+++ b03/scripts/0-create-b03-raster.R
@@ -0,0 +1,39 @@
+# a254
+# Bronwyn Rayfield and Jed Lloren, ApexRMS
+# Run with R-1.1
+
+# Workspace---------------------------------------------------------------------------------------------
+# Load packages
+library(fasterize)
+library(raster)
+library(sf)
+library(tidyverse)
+
+# Set up directories
+# Assumes these directories already exist
+rawMapsDir <- "../data/spatial"
+rawTablesDir <- "../data/tabular"
+
+# Read in data
+# Spatial
+b03Raw <- st_read(file.path(rawMapsDir,"plaine-dottawa.shp"))
+StudyExtent<-raster(file.path(rawMapsDir,"OutaouaisConnectivityExtent.tif"))
+
+# Tabular
+reclassTable <- read_csv(file.path(rawTablesDir,"b03-reclass.csv"))
+
+# Reformatting data---------------------------------------------------------------------------------------------
+# Rasterize the Plaine d'Ottawa shapefile
+studyArea <- fasterize(sf = st_cast(b03Raw, "POLYGON"), 
+                       raster = StudyExtent,
+                       field = "FID02") %>% 
+  mask(., mask=StudyExtent)
+
+# Reclassify
+b03_studyarea <- reclassify(studyArea, reclassTable)
+
+# Save outputs---------------------------------------------------------------------------------------------
+# B03 study area
+writeRaster(b03_studyarea, 
+            file.path(rawMapsDir, "b03-studyarea.tif"), 
+            overwrite = TRUE)
