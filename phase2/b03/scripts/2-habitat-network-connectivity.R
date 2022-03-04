204f3d9f5b56ced5848aaf8a703874b3cd625b76
Sarah Chisholm <89938738+sarah-chisholm@users.noreply.github.com>
2022-03-04 23:04:44 +0000

    Generate betweenness, link, and binary link maps

---
+++ phase2/b03/scripts/2-habitat-network-connectivity.R
@@ -0,0 +1,79 @@
+# a254
+# Bronwyn Rayfield, ApexRMS
+# Create betweenness and habitat netowrk maps for focal species
+#                                                                   
+# Inputs:                                                           
+#    - Habitat suitability map                                      
+#    - Resistance map                                               
+#    - Dispersal parameters                                         
+# Outputs:                                                          
+#    - Habitat network map                                          
+#    - Betweenness map  
+
+                
+# Load constants, functions, etc
+source("./b03/scripts/0-0-constants.R")
+
+# Set to a coarse resolution
+# newResolution <- 240
+
+# Workspace ----
+# Packages
+library(igraph)
+library(grainscape)
+
+# Settings
+options(stringsAsFactors=FALSE, SHAPE_RESTORE_SHX=T, useFancyQuotes = F, digits=10)
+
+for(species in speciesList){
+  
+  # Read in data
+  # focalArea <- st_read(file.path(dataDir, "polygon_projected.shp"))
+  habitatRaster <- raster(file.path(b03habitatDir, paste0(species, "_habitatPatch_Focal_Filtered_", myResolution, "m.tif")))
+  resistanceRaster <- raster(file.path(b03resistanceDir, paste0(species, "_resistance_Focal_", myResolution, "m.tif")))
+  
+  # Extract mpg ----
+  mpg <- MPG(cost = resistanceRaster, patch = habitatRaster)
+  
+  # Betweenness ----
+  btwn <- data.frame(patchId = as.numeric(vertex_attr(mpg$mpg, 'patchId')), btwn = betweenness(mpg$mpg, weights = edge_attr(mpg$mpg, 'lcpPerimWeight'), directed = FALSE))
+  #make a look-up table between patch id and betweenness value
+  btwn_lookup <- cbind(patchId = btwn$patchId, btwn = 1 / (max(btwn$btwn) - min(btwn$btwn)) * (btwn$btwn - min(btwn$btwn)))
+  #replace patch ids with betweeness values
+  btwnRaster <- reclassify(mpg$patchId, btwn_lookup)
+  
+  # Calculate overall EC ----
+  dist_mat <- shortest.paths(mpg$mpg, weights = edge_attr(mpg$mpg, 'lcpPerimWeight'))
+  
+  #Natal
+  #matrix of dispersal kernel
+  kernel_mat <- exp(-dist_mat / 459)
+  rm(dist_mat)
+  
+  #matrix of product node attributes
+  attribute_mat1 <- as.vector(as.numeric(vertex_attr(mpg$mpg, 'patchArea')))
+  attribute_mat <- attribute_mat1 %*% t(attribute_mat1)
+  
+  #matrix of PCvalues
+  PC_mat <- kernel_mat * attribute_mat
+  rm(attribute_mat)
+  
+  PCnum <- sum(PC_mat)
+  ECNatal <- sqrt(PCnum)
+  
+  # Convert link rasters to shapefiles ----
+  linkPolygon <- rasterToPolygons(mpg$lcpPerimWeight) %>% st_as_sf
+  
+  # Make binary link layers
+  lcpPerimWeightBinary <- mpg$lcpPerimWeight
+  lcpPerimWeightBinary[lcpPerimWeightBinary >= 0] <- 1
+  lcpPerimWeightBinary[is.na(lcpPerimWeightBinary)] <- 0
+  
+  # Save outputs ----
+  #geotif
+  writeRaster(btwnRaster, file.path(b03networkDir, paste0(species, "_Betweenness_", myResolution, "m.tif")), overwrite=TRUE)
+  writeRaster(mpg$lcpPerimWeight, file.path(b03networkDir, paste0(species, "_LinkLength_", myResolution, "m.tif")), overwrite=TRUE)
+  writeRaster(lcpPerimWeightBinary, file.path(b03networkDir, paste0(species, "_LinkLength_01_", myResolution, "m.tif")), overwrite=TRUE)
+  # shapefile
+  st_write(linkPolygon, dsn = b03networkDir, layer = paste0(species, "_LinkLength_", myResolution), driver = "ESRI Shapefile", append = FALSE)
+}
