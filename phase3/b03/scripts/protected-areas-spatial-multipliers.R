7db251a28ad173effb46d9c40c1e570d06a4eee1
Sarah Chisholm <89938738+sarah-chisholm@users.noreply.github.com>
2022-03-17 19:03:43 +0000

    Crop layers to b03 extent, update layer names

--- phase3/b03/scripts/protected-areas-spatial-multipliers.R
+++ phase3/b03/scripts/protected-areas-spatial-multipliers.R
@@ -15,20 +15,16 @@ source("./b03/scripts/0-constants.R")
 
 # Primary stratum
 primaryStratumRaster <- raster(file.path(b03ProcessedMapsDir, "PrimaryStratum_90m.tif"))
-
 # b03 extent
-
-
+b03Extent <- raster(file.path(b03ProcessedMapsDir, "SecondaryStratum_90m_b03Extent.tif"))
 # Protected Areas
 protectedAreasRaster <- raster(file.path(b03RawMapsDir, "AP-b03.tif"))
-
-# Areas of conservation priority
+# RMN protected areas
 rmnShapefile <- st_read(dsn = b03RawMapsDir, layer = "RMN_20210608")
-
-# Sites multicible
+# Atlas priority areas
 sitesMulticibleRawRaster <- raster(file.path(b03RawMapsDir, "sites-multicible.tif"))
 
-# Protected areas + areas of conservation priority spatial multiplier ----
+# Protected areas + RMN protected areas spatial multiplier ----
 # Rasterize rmnShapefile
 rmnRaster <- fasterize(sf = rmnShapefile,
                        raster = primaryStratumRaster,
@@ -44,7 +40,11 @@ protectedAreasRmnRawRaster[protectedAreasRmnRawRaster != 0] <- 1
 protectedAreasRmnRaster <- mask(x = protectedAreasRmnRawRaster,
                                 mask = primaryStratumRaster)
 
-# Sites multicible spatial multiplier ----
+# Crop to b03 extent
+protectedAreasRmnCroppedRaster <- crop(x = protectedAreasRmnRaster, 
+                                       y = b03Extent)
+
+# Atlas priority areas spatial multiplier ----
 sitesMulticibleRawRaster[sitesMulticibleRawRaster == 1] <- 0
 sitesMulticibleRawRaster[is.na(sitesMulticibleRawRaster)] <- 1
 
@@ -52,7 +52,11 @@ sitesMulticibleRawRaster[is.na(sitesMulticibleRawRaster)] <- 1
 sitesMulticibleRaster <- mask(x = sitesMulticibleRawRaster, 
                               mask = primaryStratumRaster)
 
-# Protected areas + areas of conservation priority + sites multicible spatial multiplier ----
+# Crop to b03 extent
+sitesMulticibleCroppedRaster <- crop(x = sitesMulticibleRaster, 
+                                     y = b03Extent)
+
+# Protected areas + RMN protected areas + atlas priority areas spatial multiplier ----
 allAreasRaster <- protectedAreasRmnRaster + sitesMulticibleRaster
 
 # Reclassify allAreasRaster
@@ -65,16 +69,26 @@ allAreasReclassifiedRaster <- reclassify(x = allAreasRaster,
                                          rcl = reclassifyMatrix) %>% 
                               mask(mask = primaryStratumRaster)
 
-# Clip layers to b03 extent ----
-
+# Crop to b03 extent
+allAreasReclassifiedCroppedRaster <- crop(x = allAreasReclassifiedRaster, 
+                                          y = b03Extent)
 
 # Save tifs to disk ----
 writeRaster(x = protectedAreasRmnRaster, 
-            filename = file.path(b03ProcessedMapsDir, "SpatialMultiplier_ProtectedAreas_RMN_b03BufferExtent.tif"),
+            filename = file.path(b03ProcessedMapsDir, "SpatialMultiplier_ProtectedAreas_b03BufferExtent.tif"),
             overwrite = TRUE)
 writeRaster(x = sitesMulticibleRaster, 
-            filename = file.path(b03ProcessedMapsDir, "SpatialMultiplier_SitesMulticible_b03BufferExtent.tif"),
+            filename = file.path(b03ProcessedMapsDir, "SpatialMultiplier_AtlasPriorityAreas_b03BufferExtent.tif"),
             overwrite = TRUE)
 writeRaster(x = allAreasReclassifiedRaster, 
-            filename = file.path(b03ProcessedMapsDir, "SpatialMultiplier_AllProtectedAreas_b03BufferExtent.tif"),
+            filename = file.path(b03ProcessedMapsDir, "SpatialMultiplier_ProtectedAreas_AtlasPriorityAreas_b03BufferExtent.tif"),
+            overwrite = TRUE)
+writeRaster(x = protectedAreasRmnCroppedRaster, 
+            filename = file.path(b03ProcessedMapsDir, "SpatialMultiplier_ProtectedAreas_b03Extent.tif"),
+            overwrite = TRUE)
+writeRaster(x = sitesMulticibleCroppedRaster, 
+            filename = file.path(b03ProcessedMapsDir, "SpatialMultiplier_AtlasPriorityAreas_b03Extent.tif"),
+            overwrite = TRUE)
+writeRaster(x = allAreasReclassifiedCroppedRaster, 
+            filename = file.path(b03ProcessedMapsDir, "SpatialMultiplier_AllProtectedAreas_AtlasPriorityAreas_b03Extent.tif"),
             overwrite = TRUE)
