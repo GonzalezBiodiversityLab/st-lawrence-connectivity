cc9f978f301f3c9b76588ad8555c36900edb25ef
Sarah Chisholm <89938738+sarah-chisholm@users.noreply.github.com>
2022-03-28 21:24:23 +0000

    Mask out b03 region before calculating MRC areas

--- phase3/b03/scripts/mrc-area.R
+++ phase3/b03/scripts/mrc-area.R
@@ -8,18 +8,38 @@ source("./b03/scripts/0-constants.R")
 
 # Workspace ----
 # Load spatial data
+# Secondary stratum layers
 secondaryStratumRaster <- raster(file.path(b03ProcessedMapsDir, "SecondaryStratum_90m.tif"))
 secondaryStratumCroppedRaster <- raster(file.path(b03ProcessedMapsDir, "SecondaryStratum_90m_b03Extent.tif"))
+
+# Primary stratum
+primaryStratumRaster <- raster(file.path(b03ProcessedMapsDir, "PrimaryStratum_90m.tif"))
 
 # Parameters
 cellSize <- 90
 
+# Mask b03 study area from secondary stratum rasters ---
+# Set primary stratum crs to match crs of secondary stratum
+crs(primaryStratumRaster) <- crs(secondaryStratumRaster)
+
+# Set b03 region (value = 3) to NA
+primaryStratumRaster[primaryStratumRaster == 3] <- NA
+
+# Crop masked primary stratum to b03 extent
+primaryStratumCroppedRaster <- crop(x = primaryStratumRaster, y = secondaryStratumCroppedRaster)
+
+# Mask secindary stratum layers
+secondaryStratumMaskedRaster <- mask(x = secondaryStratumRaster, 
+                                     mask = primaryStratumRaster)
+secondaryStratumCroppedMaskedRaster <- mask(x = secondaryStratumCroppedRaster, 
+                                            mask = primaryStratumCroppedRaster)
+
 # Get cell counts of unique values ----
-bufferExtentCounts <- freq(secondaryStratumRaster)%>% 
+bufferExtentCounts <- freq(secondaryStratumMaskedRaster)%>% 
                       as_tibble %>% 
                       rename(CellCount = count)
 
-b03ExtentCounts <- freq(secondaryStratumCroppedRaster) %>% 
+b03ExtentCounts <- freq(secondaryStratumCroppedMaskedRaster) %>% 
                        as_tibble %>% 
                        rename(CropCellCount = count)
                        
@@ -30,7 +50,7 @@ mrcArea <- bufferExtentCounts %>%
            mutate(Ratio = CropCellCount/CellCount,
                   BufferExtentArea = CellCount * cellSize^2,
                   b03ExtentArea = CropCellCount * cellSize^2) %>% 
-           filter(Ratio != 1 & Ratio != 0) %>% 
+           filter(Ratio != 1 & Ratio != 0 & !is.na(value)) %>% 
            select(value, BufferExtentArea, b03ExtentArea, Ratio) %>% 
            rename("MRC ID" = value,
                   "Buffer Extent Area (square meters)" = BufferExtentArea,
